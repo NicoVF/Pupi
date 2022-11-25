eae27c735ebcada0d0a8504101d89dc33ab2fb1b
Nico <nicolas@soybot.com>
2022-11-25 09:42:46 -0300

    Test 04, clase spreadsheet creada

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import unittest
 
 
+class Spreadsheet:
+    def __init__(self):
+        self._rows = []
+
+    def rows(self):
+        return self._rows
+
+    def add_row(self, row):
+        self._rows.append(row)
+
+
 class MyTestCase(unittest.TestCase):
     def test01_AnEmptySpreadsheetOutputsNoFile(self):
         spreadsheet = self.new_empty_spreadsheet()
@@ -8,23 +19,37 @@ class MyTestCase(unittest.TestCase):
         preowned_cars = reader.read_preowned()
         self.assertTrue(len(preowned_cars) == 0)
 
-    def test02_ANoneEmptySpreadsheetOutputAFile(self):
-        spreadsheet = self.example_spreadsheet()
+    def test02_xx(self):
+        spreadsheet = self.example_spreadsheet_with_one_row()
         reader = PreownedReaderFromSpreadsheet(spreadsheet)
         preowned_cars = reader.read_preowned()
-        self.assertTrue(len(preowned_cars) > 0)
+        self.assertTrue(len(preowned_cars) == 1)
 
     def test03_xxx(self):
+        spreadsheet = self.example_spreadsheet_with_one_row()
+        reader = PreownedReaderFromSpreadsheet(spreadsheet)
+        preowned_car = reader.read_preowned()[0]
+        self.assertEqual("Fiat", preowned_car.brand())
+
+    def test04_xxxx(self):
         spreadsheet = self.example_spreadsheet()
         reader = PreownedReaderFromSpreadsheet(spreadsheet)
         preowned_car = reader.read_preowned()[0]
         self.assertEqual("Ford", preowned_car.brand())
 
     def new_empty_spreadsheet(self):
-        return None
+        return Spreadsheet()
 
     def example_spreadsheet(self):
-        return "Hola"
+        spreadsheet = Spreadsheet()
+        spreadsheet.add_row("Ford")
+        return spreadsheet
+
+    def example_spreadsheet_with_one_row(self):
+        spreadsheet = Spreadsheet()
+        spreadsheet.add_row("Fiat")
+        return spreadsheet
+
 
 
 if __name__ == '__main__':
@@ -32,9 +57,11 @@ if __name__ == '__main__':
 
 
 class PreownedCar:
+    def __init__(self, marca):
+        self._marca = marca
 
     def brand(self):
-        return "Ford"
+        return self._marca
 
 
 class PreownedReaderFromSpreadsheet:
@@ -42,6 +69,8 @@ class PreownedReaderFromSpreadsheet:
         self._spreadsheet = spreadsheet
 
     def read_preowned(self):
-        if self._spreadsheet is None:
-            return []
-        return [PreownedCar()]
+        preowned_cars = []
+        for row in self._spreadsheet.rows():
+            preowned_cars.append(PreownedCar(row))
+        return preowned_cars
+

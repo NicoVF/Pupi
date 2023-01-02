b1bdb9d253ccca1881e7d6264a074b17967e3b6a
Nico <nicolas@soybot.com>
2023-01-02 14:56:01 -0300

    modificamos metodo convert csv a xml

--- Pupi/Pupi_interface/business/pupi.py
+++ Pupi/Pupi_interface/business/pupi.py
@@ -8,9 +8,11 @@ class Pupi:
     def convert_to_xml(self, csv):
         rows = csv.splitlines()
         marca_elements = ""
+        last_row = ""
         for row in rows:
-            marca_element = self._convert_row_to_xml(row)
+            marca_element = self._convert_row_to_xml(row, last_row)
             marca_elements += marca_element
+            last_row = row
         converted_xml = f"<?xml version='1.0' encoding='utf-8'?>\
             <marcas xmlns='http://chat.soybot.com/catalogo/V1'>\
 {marca_elements}\
@@ -18,15 +20,19 @@ class Pupi:
         "
         return converted_xml
 
-    def _convert_row_to_xml(self, row):
+    def _convert_row_to_xml(self, row, previous_row):
         fields = row.split(',')
-        marca = fields[0]
+        previous_fields = previous_row.split(',')
+        current_brand = fields[0]
+        previous_brand = previous_fields[0]
+        if previous_brand == current_brand:
+            return ""
         modelo_elemento = ""
         if len(fields) > 1:
             modelo = fields[1]
             modelo_elemento = f"                    <modelo display='{modelo}' estado='activo'>" + \
                               "                    </modelo>"
 
-        return f"                <marca nombre='{marca}' estado='activo'>\
+        return f"                <marca nombre='{current_brand}' estado='activo'>\
 {modelo_elemento}\
                 </marca>"

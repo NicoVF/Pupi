93aa4610b42312c288a1e4dccbf6e729055cfdb6
Maxipanza <78429597+Maxipanza@users.noreply.github.com>
2023-01-10 12:11:44 -0300

    refactor unidad
    ahora usamos la unidad en vez de los fields

--- Pupi/Pupi_interface/business/pupi.py
+++ Pupi/Pupi_interface/business/pupi.py
@@ -2,15 +2,29 @@ import xml.etree.ElementTree as ET
 
 
 class UnitForSale:
-    def __init__(self, brand, model):
+    def __init__(self, brand, model, version, id):
         self._brand = brand
         self._model = model
+        self._version = version
+        self._id = id
 
     def brand(self):
         return self._brand
 
     def model(self):
         return self._model
+
+    def version(self):
+        return self._version
+
+    def id(self):
+        return self._id
+
+    def has_id(self):
+        return self._id
+
+    def has_valid_brand(self):
+        return self._brand is not None
 
 
 class Pupi:
@@ -21,11 +35,13 @@ class Pupi:
     def convert_to_xml(self, csv):
         rows = csv.splitlines()
         brands = ET.Element("marcas", xmlns='http://chat.soybot.com/catalogo/V1')
-        previous_unit_for_sale = UnitForSale(brand=None, model=None)
+        previous_unit_for_sale = UnitForSale(brand=None, model="", version=None, id=None)
         for row in rows:
             fields = row.split(',')
 
-            unit_for_sale = UnitForSale(brand=fields[0], model=fields[1] if len(fields) > 1 else "")
+            unit_for_sale = UnitForSale(brand=fields[0], model=fields[1] if len(fields) > 1 else "",
+                                        version=fields[2] if len(fields) > 2 and fields[2] != "" else None,
+                                        id=fields[6] if len(fields) > 6 else None)
 
             brand_node_must_be_inserted = unit_for_sale.brand() != previous_unit_for_sale.brand()
             if brand_node_must_be_inserted:
@@ -36,11 +52,15 @@ class Pupi:
                 model = ET.SubElement(brand, "modelo", display=unit_for_sale.model(), estado='activo')
                 last_valid_parent_for_unit_element = model
 
-            
+            version_node_must_be_inserted = unit_for_sale.version() is not None and\
+                                            unit_for_sale.version() != previous_unit_for_sale.version()
+            if version_node_must_be_inserted:
+                version = ET.SubElement(model, "version", display=unit_for_sale.version(), estado='activo')
+                last_valid_parent_for_unit_element = version
 
-            unit_node_must_be_inserted = self._must_insert_unit_element(fields)
+            unit_node_must_be_inserted = self._must_insert_unit_element(unit_for_sale)
             if unit_node_must_be_inserted:
-                self._create_unit_element(last_valid_parent_for_unit_element, fields)
+                self._create_unit_element(last_valid_parent_for_unit_element, unit_for_sale)
 
             previous_unit_for_sale = unit_for_sale
         ET.indent(brands, space='    ')
@@ -86,17 +106,17 @@ class Pupi:
         xml = ET.tostring(brands, encoding="utf-8", method='xml', xml_declaration=True, ).decode('utf-8')
         return xml
 
-    def _unit_data_exists(self, fields):
-        return len(fields) > 0
+    def _unit_data_exists(self, unit_for_sale):
+        return unit_for_sale.has_valid_brand()
 
-    def _create_unit_element(self, parent_node, fields):
-        if len(fields) > 6:
-            ET.SubElement(parent_node, "unidad", id=fields[6])
+    def _create_unit_element(self, parent_node, unit_for_sale):
+        if unit_for_sale.has_id():
+            ET.SubElement(parent_node, "unidad", id=unit_for_sale.id())
         else:
             ET.SubElement(parent_node, "unidad")
 
-    def _must_insert_unit_element(self, fields):
+    def _must_insert_unit_element(self, unit_for_sale):
         cuando_no_existe_una_unidad_falopa = True
-        return cuando_no_existe_una_unidad_falopa and self._unit_data_exists(fields)
+        return cuando_no_existe_una_unidad_falopa and self._unit_data_exists(unit_for_sale)
 
 

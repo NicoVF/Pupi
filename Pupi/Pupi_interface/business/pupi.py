ac7dcfcea6d4223a4728ef72abfa587f4d3f790b
Nico <nicolas@soybot.com>
2023-01-11 14:48:00 -0300

    refactor set unit for sale

--- Pupi/Pupi_interface/business/pupi.py
+++ Pupi/Pupi_interface/business/pupi.py
@@ -29,6 +29,17 @@ class UnitForSale:
     def has_valid_brand(self):
         return self._brand is not None
 
+    @classmethod
+    def no_unit_for_sale(cls):
+        return cls(brand=None, model="", version=None, id=None)
+
+    @classmethod
+    def create_unit_from(cls, fields):
+        return cls(brand=fields[0],
+                   model=fields[1] if len(fields) > 1 else "",
+                   version=fields[2] if len(fields) > 2 and fields[2] != "" else None,
+                   id=fields[6] if len(fields) > 6 else None)
+
 
 class Pupi:
 
@@ -38,34 +49,32 @@ class Pupi:
     def convert_to_xml(self, csv):
         rows = csv.splitlines()
         brands = ET.Element("marcas", xmlns='http://chat.soybot.com/catalogo/V1')
-        previous_unit_for_sale = UnitForSale(brand=None, model="", version=None, id=None)
+        previous_unit_for_sale = UnitForSale.no_unit_for_sale()
         for row in rows:
             fields = row.split(',')
 
-            unit_for_sale = UnitForSale(brand=fields[0], model=fields[1] if len(fields) > 1 else "",
-                                        version=fields[2] if len(fields) > 2 and fields[2] != "" else None,
-                                        id=fields[6] if len(fields) > 6 else None)
+            current_unit_for_sale = UnitForSale.create_unit_from(fields)
 
-            brand_node_must_be_inserted = unit_for_sale.brand() != previous_unit_for_sale.brand()
+            brand_node_must_be_inserted = current_unit_for_sale.brand() != previous_unit_for_sale.brand()
             if brand_node_must_be_inserted:
-                brand = ET.SubElement(brands, "marca", nombre=unit_for_sale.brand(), estado='activo')
+                brand = ET.SubElement(brands, "marca", nombre=current_unit_for_sale.brand(), estado='activo')
 
-            model_node_must_be_inserted = brand_node_must_be_inserted or unit_for_sale.model() != previous_unit_for_sale.model()
+            model_node_must_be_inserted = brand_node_must_be_inserted or current_unit_for_sale.model() != previous_unit_for_sale.model()
             if model_node_must_be_inserted:
-                model = ET.SubElement(brand, "modelo", display=unit_for_sale.model(), estado='activo')
+                model = ET.SubElement(brand, "modelo", display=current_unit_for_sale.model(), estado='activo')
                 last_valid_parent_for_unit_element = model
 
-            version_node_must_be_inserted = unit_for_sale.version() is not None and\
-                                            unit_for_sale.version() != previous_unit_for_sale.version()
+            version_node_must_be_inserted = current_unit_for_sale.version() is not None and\
+                                            current_unit_for_sale.version() != previous_unit_for_sale.version()
             if version_node_must_be_inserted:
-                version = ET.SubElement(model, "version", display=unit_for_sale.version(), estado='activo')
+                version = ET.SubElement(model, "version", display=current_unit_for_sale.version(), estado='activo')
                 last_valid_parent_for_unit_element = version
 
-            unit_node_must_be_inserted = self._must_insert_unit_element(unit_for_sale, previous_unit_for_sale)
+            unit_node_must_be_inserted = self._must_insert_unit_element(current_unit_for_sale, previous_unit_for_sale)
             if unit_node_must_be_inserted:
-                self._create_unit_element(last_valid_parent_for_unit_element, unit_for_sale)
+                self._create_unit_element(last_valid_parent_for_unit_element, current_unit_for_sale)
 
-            previous_unit_for_sale = unit_for_sale
+            previous_unit_for_sale = current_unit_for_sale
         ET.indent(brands, space='    ')
         xml = ET.tostring(brands, encoding="utf-8", method='xml', xml_declaration=True, ).decode('utf-8')
         return xml

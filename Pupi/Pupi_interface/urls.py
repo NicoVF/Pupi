33d6f378aacb562d05ddef4f50565f1dad4d2908
Maxipanza <maximiliano@run0km.com>
2023-03-01 11:14:28 -0300

    cambiamos el orden de las url

--- Pupi/Pupi_interface/urls.py
+++ Pupi/Pupi_interface/urls.py
@@ -4,7 +4,7 @@ from .views import SendCatalogView, ConvertCSVView, NormalizeAndSortCSVView
 
 app_name = 'polls'
 urlpatterns = [
-    path('catalog/send', SendCatalogView.as_view(), name='SendCatalog'),
-    path('catalog/convert', ConvertCSVView.as_view(), name='ConvertCSV'),
     path('catalog/normalizeAndSort', NormalizeAndSortCSVView.as_view(), name='NormalizeAndSortCSV'),
+    path('catalog/convert', ConvertCSVView.as_view(), name='ConvertCSV'),
+    path('catalog/send', SendCatalogView.as_view(), name='SendCatalog'),
 ]

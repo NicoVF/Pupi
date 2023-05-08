b3822e1739b639f7ca9d9d18b79e38889f881b31
Nico <nicolas@soybot.com>
2023-05-08 19:40:47 -0300

    API - unidades a traves de localizacion

--- Pupi/Pupi_interface/urls.py
+++ Pupi/Pupi_interface/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import SendCatalogView, ConvertCSVView, NormalizeAndSortCSVView
+from .views import SendCatalogView, ConvertCSVView, NormalizeAndSortCSVView, GetUnitsWithLocalizationArguments
 
 app_name = 'polls'
 urlpatterns = [
     path('catalog/normalizeAndSort', NormalizeAndSortCSVView.as_view(), name='NormalizeAndSortCSV'),
     path('catalog/convert', ConvertCSVView.as_view(), name='ConvertCSV'),
     path('catalog/send', SendCatalogView.as_view(), name='SendCatalog'),
+    path('catalog/getUnitsWithLocalizationArguments', GetUnitsWithLocalizationArguments.as_view(),
+         name="getUnitsWithLocalizationArguments")
+
 ]
